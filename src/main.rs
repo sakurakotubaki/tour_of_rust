6c27476f79087e638a1a537b237fc19d7ce9136d
sakurakotubaki <gitonly543@gmail.com>
2025-06-01 16:41:09 +0900

    added: data type

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    let mut x = 42;
-    println!("{}", x);
-    x = 13;
-    println!("{}", x);
+    let x = 12; // デフォルトでは i32
+    let a = 12u8;
+    let b = 4.3; // デフォルトでは f64
+    let c = 4.3f32;
+    let bv = true;
+    let t = (13, false);
+    let sentence = "hello world!";
+    println!(
+        "{} {} {} {} {} {} {} {}",
+        x, a, b, c, bv, t.0, t.1, sentence
+    );
 }
