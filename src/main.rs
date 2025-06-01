e0479fe86c7def1ad973f9f675ffdfbe2f864cbc
sakurakotubaki <gitonly543@gmail.com>
2025-06-01 16:48:16 +0900

    edit as key word

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 fn main() {
-    let x = 12; // デフォルトでは i32
-    let a = 12u8;
-    let b = 4.3; // デフォルトでは f64
-    let c = 4.3f32;
-    let bv = true;
-    let t = (13, false);
-    let sentence = "hello world!";
-    println!(
-        "{} {} {} {} {} {} {} {}",
-        x, a, b, c, bv, t.0, t.1, sentence
-    );
+    let a = 13u8;
+    let b = 7u32;
+    let c = a as u32 + b;
+    println!("{}", c);
+
+    let t = true;
+    println!("{}", t as u8);
 }
