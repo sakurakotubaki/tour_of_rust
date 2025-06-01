71db1ad48e6de84bbd12f356fa41908a2f0a177c
sakurakotubaki <gitonly543@gmail.com>
2025-06-01 16:36:11 +0900

    added: mut

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
 fn main() {
-    // x の型を推論
-    let x = 13;
+    let mut x = 42;
     println!("{}", x);
-
-    // x の型を指定
-    let x: f64 = 3.14159;
-    println!("{}", x);
-
-    // 宣言の後で初期化（あまり使われません）
-    let x;
-    x = 0;
+    x = 13;
     println!("{}", x);
 }
